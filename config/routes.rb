be942f8d6bf85456cc9dce56d476149523fdce89
quentcol <quent.col@gmail.com>
2024-02-06 20:18:37 +0100

    I have succesfully implemented everything but have not taken care of the front-end

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
